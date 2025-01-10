56af2a55bcafd19218ff67f40a1d21381bb1fe2a
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-10 13:53:31 +0900

    Title: 되돌아오기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250110/되돌아오기/come-back.py
@@ -0,0 +1,30 @@
+n=int(input())
+
+
+dx={'E':1,'N':0,'W':-1,'S':0}
+
+dy={'E':0,'N':1,'W':0,'S':-1}
+
+
+x,y=0,0
+
+cnt=0
+cnt1=0
+for i in range(n):
+    d,num=tuple(input().split())
+    num=int(num)
+
+    for j in range(num):
+        x,y=x+dx[d],y+dy[d]
+        
+        cnt+=1
+        if x==0 and y==0:
+            cnt1=cnt
+
+
+    
+if cnt1==0:
+    print(-1)
+else:
+    print(cnt1)
+
