4595a6f1069128ba055458520baf8911f00f8e9c
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-10 10:24:51 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250110/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250110/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -20,10 +20,7 @@ arr2.sort(key=lambda x:x.time)
 for i in range(T):
     x1=arr2[i].x
     y1=arr2[i].y
-    
-    if arr1[x1-1]==0:
-        continue
-    else:
+
         if (arr[x1-1]==1 and arr[y1-1]==0) :
             if arr1[x1-1]==0:
                 continue
