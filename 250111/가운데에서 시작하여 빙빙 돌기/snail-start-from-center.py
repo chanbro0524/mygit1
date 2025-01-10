ca9e15fd7f58416c1b19939324d72bb5f6897a53
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-11 01:03:13 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250111/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 250111/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -7,16 +7,18 @@ arr=[[0]*n for i in range(n)]
 x,y=n//2,n//2
 dx=[0,-1,0,1]
 dy=[1,0,-1,0]
-d=0
-
+d=3
+def indirect(x,y):
+    if x>=0 and y>=0 and x<=n-1 and y<=n-1:
+        return True
 
 for i in range(1,n*n+1):
     arr[x][y]=i
     d1=(d+1)%4
-    x,y=x+dx[d1],y+dy[d1]
-    if arr[x][y]!=0:
+    x1,y1=x+dx[d1],y+dy[d1]
+    if arr[x1][y1]!=0:
         x,y=x+dx[d],y+dy[d]
-    else:
+    elif arr[x1][y1]==0:
         d=(d+1)%4
         x,y=x+dx[d],y+dy[d]
 
@@ -24,6 +26,6 @@ for i in range(1,n*n+1):
 
 
 for i in range(n):
-    for j in range(m):
+    for j in range(n):
         print(arr[i][j],end=" ")
     print()
