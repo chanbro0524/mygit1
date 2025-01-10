74a04e56402a7e0ec84771ed88a40a4a9822bc04
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-11 01:04:43 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 67ms, Memory: 18MB, Status: Passed - CodeTree

--- 250111/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 250111/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -18,7 +18,7 @@ for i in range(1,n*n+1):
     x1,y1=x+dx[d1],y+dy[d1]
     if indirect(x1,y1) and arr[x1][y1]!=0:
         x,y=x+dx[d],y+dy[d]
-    elif arr[x1][y1]==0:
+    elif indirect(x1,y1) and arr[x1][y1]==0:
         d=(d+1)%4
         x,y=x+dx[d],y+dy[d]
 
