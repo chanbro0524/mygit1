813b3f77b108c088b68ed0a15aba279c9dcd4383
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-10 10:41:52 +0900

    Title: 선두를 지켜라 3, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250110/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,47 @@
+n,m=tuple(map(int,input().split()))
+arr=[]
+d=0
+for i in range(n):
+    v,t=tuple(map(int,input().split()))
+    for j in range(t):
+        d+=v
+        arr.append(d)
+arr1=[]
+d=0
+for i in range(m):
+    v,t=tuple(map(int,input().split()))
+    for j in range(t):
+        d+=v
+        arr1.append(d)
+arr3=[]
+lead=0 
+cnt=0
+for i in range(len(arr)):
+    if arr[i]>arr1[i]:
+        arr3.append("A")
+    elif arr[i]<arr1[i]:
+        arr3.append("B")
+    elif arr[i]==arr1[i]:
+        arr3.append("S")
+
+
+cnt=1
+for i in range(len(arr3)-1):
+    if arr3[i]!=arr3[i+1]:
+        cnt+=1
+    
+print(cnt)
+    
+print(arr3)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
