e8b40764ed67cd790a03433026949700537ed9b2
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-10 10:04:05 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 75ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250110/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,38 @@
+n,k,p,T=tuple(map(int,input().split()))
+arr=[0,0,0,0]
+arr1=[2,2,2,2]
+arr[p-1]=1
+
+class Hand:
+    def __init__(self,time,x,y):
+        self.time=time
+        self.x=x
+        self.y=y
+
+
+arr2=[]
+for i in range(T):
+    t,x,y=tuple(map(int,input().split()))
+    arr2.append(Hand(t,x,y))
+
+
+arr2.sort(key=lambda x:x.time)
+for i in range(T):
+    x1=arr2[i].x
+    y1=arr2[i].y
+    
+    if arr1[x1-1]==0:
+        continue
+    else:
+        if (arr[x1-1]==1 and arr[y1-1]==0) :
+            arr[y1-1]=1
+            arr1[x1-1]-=1
+    if arr1[y1-1]==0:
+        continue
+    else:
+        if (arr[x1-1]==0 and arr[y1-1]==1):
+            arr[x1-1]=1
+
+
+for i in arr:
+    print(i,end="")
