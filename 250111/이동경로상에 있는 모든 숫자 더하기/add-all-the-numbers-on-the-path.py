ef68e0a74fd69f807becd906f258fe60c6076e0b
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-11 01:29:58 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 81ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250111/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -0,0 +1,34 @@
+n,t=tuple(map(int,input().split()))
+order=input()
+
+arr=[list(map(int,input().split())) for i in range(3)]
+
+dx=[-1,0,1,0]
+dy=[0,-1,0,1]
+d=0
+x,y=n//2,n//2
+cnt=0
+def inrange(x,y):
+    if x>=0 and y>=0 and x<n and y<n:
+        return True
+
+        
+arr2=[]   
+for i in range(t):
+    o=order[i]
+    if o=='L':
+        d=(d+1)%4
+    elif o=="R":
+        d=(d+3)%4
+    elif o=="F":
+        x1,y1=x+dx[d],y+dy[d]
+        if inrange(x1,y1):
+            x,y=x+dx[d],y+dy[d]
+        else:
+            continue
+    if arr[x][y] not in arr2:
+        arr2.append(arr[x][y])
+cnt=0
+for i in arr2:
+    cnt+=i
+print(cnt)
