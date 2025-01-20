ccb10cfcb7b408b25a7c5d4ecfdeff3baf9ab533
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-20 14:18:47 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 57ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250120/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 250120/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -3,11 +3,15 @@ points = [tuple(map(int, input().split())) for _ in range(n)]
 x = [p[0] for p in points]
 y = [p[1] for p in points]
 import sys
-num=sys.maxsize()
+num=sys.maxsize
 for i in range(1,n):
     x = [p[0] for p in points]
     y = [p[1] for p in points]
     del x[1]
     del y[1]
+    dis=0
     for j in range(len(x)-1):
-        dis=x[j]
+        dis+=abs(x[j]-x[j+1])+abs(y[j]-y[j+1])
+    num=min(num,dis)
+
+print(num)
