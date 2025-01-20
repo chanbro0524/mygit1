ecd9502b34138b18c71837445997e7ecde7aae79
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-20 14:26:43 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250120/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 250120/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -7,8 +7,8 @@ num=sys.maxsize
 for i in range(1,n):
     x = [p[0] for p in points]
     y = [p[1] for p in points]
-    del x[1]
-    del y[1]
+    del x[i]
+    del y[i]
     dis=0
     for j in range(len(x)-1):
         dis+=abs(x[j]-x[j+1])+abs(y[j]-y[j+1])
