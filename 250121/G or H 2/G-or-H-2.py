8aa942031cd15da78b06a9b69799c25dfb2ed3e1
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-21 13:46:33 +0900

    Title: G or H 2, Time: 53ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250121/G or H 2/G-or-H-2.py
@@ -0,0 +1,19 @@
+n = int(input())
+people = [tuple(input().split()) for _ in range(n)]
+pos = [int(p[0]) for p in people]
+alpha = [p[1] for p in people]
+arr1=[0]*100
+for i,j in zip(pos,alpha):
+    arr1[i]=j
+
+
+num=0
+if len(alpha)==1:
+    print(num)
+else:
+    for i in range(100):
+        for j in range(i,100):
+            arr=arr1[i:j+1]
+            if arr.count('G')==arr.count('H') and (arr1[i]=='G' or arr1[i]=='H') and (arr1[j]=='G' or arr1[j]=='H'):
+               num=max(num,j-i)
+    print(num)
