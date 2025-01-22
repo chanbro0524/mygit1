fb4adfb268c6740e890fb2a3f94e753c526c7aef
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-22 15:02:43 +0900

    Title: 스승의 은혜 2, Time: 59ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250122/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,18 @@
+N, B = map(int, input().split())
+P = [int(input()) for _ in range(N)]
+
+int_max=0
+for i in range(N):
+    count=0
+    s=P[i]/2
+    if s>B:
+        break
+    count+=1
+    for j in range(N):
+        if i==j:
+            continue
+        if s+P[j]<=B:
+            count+=1
+            s=s+P[j]
+    int_max=max(int_max,count)
+print(count)
