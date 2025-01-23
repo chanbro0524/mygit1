d206e7027c7bcff16a9e250c915bca5eee5fb340
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-23 12:03:44 +0900

    Title: 이상한 폭탄 2, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 250123/이상한 폭탄 2/strange-bomb-2.py
+++ 250123/이상한 폭탄 2/strange-bomb-2.py
@@ -1,7 +1,7 @@
 N, K = map(int, input().split())
 arr = [int(input()) for _ in range(N)]
 
-int_max=1
+int_max=0
 for i in range(N):
     for j in range(N):
         if j==i:
@@ -9,4 +9,6 @@ for i in range(N):
         if arr[i]==arr[j]:
             if abs(i-j)<=K:
                 int_max=max(int_max,arr[i])
+    if int_max==0:
+        int_max=-1
 print(int_max)
