da6e1ce635b2aedd7c340a67ab0a87b788449f30
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-30 17:27:45 +0900

    Title: 이상한 폭탄 3, Time: 54ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 250130/이상한 폭탄 3/strange-bomb-3.py
+++ 250130/이상한 폭탄 3/strange-bomb-3.py
@@ -4,7 +4,7 @@ arr=[0]*1000000
 for i in range(N-1):
     for j in range(i+1,N):
         if num[i]==num[j]:
-            if j-i<K:
+            if j-i<=K:
                 arr[num[i]]+=1
 
 print(arr.index(max(arr)))
