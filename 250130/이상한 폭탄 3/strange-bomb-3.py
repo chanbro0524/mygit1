d2b624c6237ebc92200072caabd70ba96fb2ebb2
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-30 17:25:28 +0900

    Title: 이상한 폭탄 3, Time: 50ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 250130/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,10 @@
+N, K = map(int, input().split())
+num = [int(input()) for _ in range(N)]
+arr=[0]*1000000
+for i in range(N-1):
+    for j in range(i+1,N):
+        if num[i]==num[j]:
+            if j-i<K:
+                arr[num[i]]+=1
+
+print(max(arr))
