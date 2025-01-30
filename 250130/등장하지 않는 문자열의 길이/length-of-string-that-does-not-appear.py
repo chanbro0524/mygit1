7db485b84cc4bc77eee2237f39d5d2231646b4e1
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-01-30 15:41:16 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250130/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 250130/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -4,9 +4,9 @@ intmin=N
 for i in range(N):
     num=0
     for j in range(N-i):
-        if str[j:j+i] in str[j+i+1:]:
+        if str[j:j+i] in str[j+1:]:
             num+=1
-    if num!=0:
+    if num>0:
         continue
     intmin=min(i,intmin)
 print(intmin)
