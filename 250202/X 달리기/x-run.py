c4b537dc1a40508a71c9f134d7532b49553bf236
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-02-02 02:08:13 +0900

    Title: X 달리기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250202/X 달리기/x-run.py
+++ 250202/X 달리기/x-run.py
@@ -5,12 +5,12 @@ t=0
 d=0
 ms=1
 for i in range(5000):
-    if (d+ms)*2<X:
+    if d + 2 * ms < X:
         t+=2
         d+=2*ms
         ms+=1
 
-    elif (d+ms)*2==X:
+    elif d + 2 * ms == X:
         t+=2
         ms+=1
 
