799844ee2d6514b8940e12c958120aa4fcee8259
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-02-02 00:19:10 +0900

    Title: 코딩톡, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250202/코딩톡/coding-talk.py
@@ -0,0 +1,18 @@
+n, m, p = map(int, input().split())
+messages = [tuple(input().split()) for _ in range(m)]
+c = [msg[0] for msg in messages]
+u = [int(msg[1]) for msg in messages]
+
+# Write your code here!
+
+letter=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+letters=letter[:n]
+
+
+for i in range(p-1,n):
+    if c[i] in letters:
+        letters.remove(c[i])
+
+for j in letters:
+    print(j,end=' ')
