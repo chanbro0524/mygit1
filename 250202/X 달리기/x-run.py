76ec995df83c926d24216feec20bf8dcb6c2445b
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-02-02 02:02:02 +0900

    Title: X 달리기, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250202/X 달리기/x-run.py
+++ 250202/X 달리기/x-run.py
@@ -4,16 +4,27 @@ X = int(input())
 t=0
 d=0
 ms=1
-for i in range(10000):
-    if d+2*ms<X:
+for i in range(5000):
+    if (d+ms)*2<X:
+        t+=2
         d+=2*ms
+        ms+=1
+
+    elif (d+ms)*2==X:
         t+=2
         ms+=1
+
+        break
     else:
-        if d+ms<X:
-            d+=ms
+        if (d+ms)<X:
             t+=1
-            ms+=1
+            t+=(X-(d+ms))
+
+            break
+        elif (d+ms)==X:
+            t+=1
+
+            break
         else:
             t+=X-d
 
