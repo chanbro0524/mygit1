4a401c8bba76a5b7efae6aea0f6e651fcc8cfe25
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-02-01 20:30:03 +0900

    Title: 전부 겹치는 선분 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250201/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -0,0 +1,22 @@
+n = int(input())
+segments = [tuple(map(int, input().split())) for _ in range(n)]
+x1 = [seg[0] for seg in segments]
+x2 = [seg[1] for seg in segments]
+maxx2=1
+minx1=100
+num=0
+arr=[0]*101
+for i in range(n):
+    for j in range(n):
+        if i==j:
+            continue
+        x=x1[j]
+        y=x2[j]
+        for k in range(x,y+1):
+            arr[k]+=1
+    if arr.count(n)>=1:
+        print('Yes')
+        num+=1
+        break
+if num==0:    
+    print('No')
