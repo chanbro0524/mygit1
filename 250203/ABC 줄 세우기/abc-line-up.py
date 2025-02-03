fa1b3f4522ac012c97796676480ba125662f84e2
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-02-03 15:55:26 +0900

    Title: ABC 줄 세우기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250203/ABC 줄 세우기/abc-line-up.py
+++ 250203/ABC 줄 세우기/abc-line-up.py
@@ -3,14 +3,17 @@ arr = list(input().split())
 arr1=[arr[i] for i in range(n)]
 arr.sort()
 num=0
-for i in range(n):
-    for j in range(n):
+for j in range(n):
+    for i in range(n):
+        k=i
         if arr1[i]==arr[j]:
-            if i==j:
-                break
-            t=arr1[i-1]
-            arr1[i-1]=arr[i]
-            arr[i]=t
-            num+=1
+            for l in range(n):
+                if k==j:
+                    break
+                t=arr1[i-1]
+                arr1[i-1]=arr[i]
+                arr[i]=t
+                k-=1
+                num+=1
             break
 print(num)
