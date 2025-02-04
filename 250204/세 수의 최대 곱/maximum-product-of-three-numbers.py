a18e0c6de3f47147770f368de07d03414c62c1f9
chanbro0524 <164038275+chanbro0524@users.noreply.github.com>
2025-02-04 10:51:57 +0900

    Title: 세 수의 최대 곱, Time: 77ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250204/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -0,0 +1,23 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Write your code here!f
+arr1=[]
+arr2=[]
+for i in range(n):
+    if arr[i]>0:
+        arr1.append(arr[i])
+    elif arr[i]<0:
+        arr2.append(arr[i])
+
+
+if len(arr2)==1 or len(arr1)==0:
+    if 0 in arr:
+        print(0)
+    else:
+        arr2.sort(reverse=True)
+        print(arr2[0]*arr2[1]*arr2[2])
+else:
+    arr1.sort(reverse=True)
+    arr2.sort()
+    print(arr1[0]*arr2[0]*arr2[1])
